119b00e2b80f59049c99f710a637c30edfbc6bab
ethben0809 <ethben0809@highgateschool.org.uk>
2025-01-20 14:46:18 +0000

    more data inputting

--- WriteToStudentDatabase.py
+++ WriteToStudentDatabase.py
@@ -1,25 +1,33 @@
 
-
-
+import random
+from faker import Faker
 import sqlite3
 
+fake = Faker('en_GB')
 conn = sqlite3.connect('students.sqlite')
 
-with sqlite3.connect('students.sqlite') as conn:
-    cursor = conn.cursor()
-    insert_query = """
-    INSERT INTO students (firstname,lastname ,age,gender)
-     VALUES 
-            ('Milan','Gal',69,'Eye of Rah'),
-            ('Eleanore','Shiner',1000,'amogus'),
-            ('Rayhan','Chowdhury','Eternal','Existence'),
-            ('Denys','Zazuliak',0,'GOD'),
-            ('Adam','Reeves',16,'Male'),
-            ('Sami','Hafezgi',100,'Soliaire');
+parameterised_insert_query="""
+INSERT INTO 
+    students (firstname, lastname, age, gender)
+values 
+    (?, ?, ?, ?)
+"""
 
-    """
-    cursor.execute(insert_query)
+conn=sqlite3.connect('students.sqlite')
+cursor = conn.cursor()
 
-    conn.commit()
+fake.random.seed(42)
+random.seed(42)
 
+for _ in range(10):
+    f_name = fake.first_name()
+    l_name = fake.last_name()
+    age=random.randint(10,20)
+    gender=random.choice(['M','F','NB','O'])
+    cursor.execute(parameterised_insert_query,(f_name,l_name,age,gender))
+
+
+
+conn.commit()
+conn.close()
 
