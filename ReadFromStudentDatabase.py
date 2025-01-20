2b1982778a16e0e4117462f81448231b11cd36ba
ethben0809 <ethben0809@highgateschool.org.uk>
2025-01-20 15:53:50 +0000

    read more

--- ReadFromStudentDatabase.py
+++ ReadFromStudentDatabase.py
@@ -19,7 +19,7 @@ select avg(age)
 from students
 where gender=?
 """
-average_age= cursor.execute(average_query,("F",)).fetchone()[0]
+average_age= cursor.execute(average_query,("F",)).fetchall()[0][0]
 
 group_by_query="""
 Select gender, avg(age)
@@ -30,10 +30,38 @@ group by gender
 average_age_by_gender= cursor.execute(group_by_query).fetchall()
 
 
+j_start_query="""
+select firstname
+from students
+where firstname like 'J%'
+"""
 
+j_start_response=cursor.execute(j_start_query).fetchall()[0:5]
+
+
+number_per_gender_query="""
+select gender,count(gender)
+from students
+group by gender"""
+
+gender_count=cursor.execute(number_per_gender_query).fetchall()
+
+number_firstname_count_query="""
+select firstname, count(firstname)
+from students
+group by substring(firstname,1,1)
+"""
+
+name_count_result=cursor.execute(number_firstname_count_query).fetchall()
 conn.close()
 
 print(first_student,more_students,other_students,sep='\n', end="\n\n")
 
 print(average_age)
-print(average_age_by_gender)
+print(sorted(average_age_by_gender,key=lambda s:s[1]))
+
+print(j_start_response)
+
+print(sorted(gender_count,key=lambda s:s[1]))
+
+print(name_count_result)
