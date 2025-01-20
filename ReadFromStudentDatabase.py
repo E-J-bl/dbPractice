75e7e41042aa76335a7f6fe4dd3e4645f5160d45
ethben0809 <ethben0809@highgateschool.org.uk>
2025-01-20 15:38:42 +0000

    read more

--- ReadFromStudentDatabase.py
+++ ReadFromStudentDatabase.py
@@ -12,6 +12,28 @@ cursor.execute(select_students)
 first_student = cursor.fetchone()
 more_students=cursor.fetchmany(5)
 other_students=cursor.fetchall()
+
+
+average_query="""
+select avg(age)
+from students
+where gender=?
+"""
+average_age= cursor.execute(average_query,("F",)).fetchone()[0]
+
+group_by_query="""
+Select gender, avg(age)
+from students
+group by gender
+"""
+
+average_age_by_gender= cursor.execute(group_by_query).fetchall()
+
+
+
 conn.close()
 
-print(first_student,more_students,other_students,sep='\n')
+print(first_student,more_students,other_students,sep='\n', end="\n\n")
+
+print(average_age)
+print(average_age_by_gender)
